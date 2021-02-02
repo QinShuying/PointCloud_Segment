a7d39e8b3f227521821e58c87318d5a0bdd41a56
Qinshuying <qsy20000310@126.com>
2021-02-02 23:49:09 +0800

    未定义的引用

---
+++ include/datapretreat.h
@@ -0,0 +1,16 @@
+#ifndef POINTCLOUD_SEGMENT_DATAPRETREAT_H
+#define POINTCLOUD_SEGMENT_DATAPRETREAT_H
+
+#include "seg.h"
+
+class datapretreat {
+
+public:
+    void ReadData(std::string &in_file, pcl::PointCloud<pcl::PointXYZ>::Ptr cloud);
+
+    void test(void);
+
+};
+
+
+#endif //POINTCLOUD_SEGMENT_DATAPRETREAT_H
